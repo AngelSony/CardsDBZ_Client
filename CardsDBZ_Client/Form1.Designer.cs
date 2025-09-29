51089a8fd99caa54aa2578ad4fb4b675a74d6578
AngelSony <angelfol@outlook.com>
2025-09-29 00:51:25 -0300

    Agregar archivos de proyecto.

---
+++ CardsDBZ_Client/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CardsDBZ_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(306, 154);
+            label1.Name = "label1";
+            label1.Size = new Size(184, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Me muevo mira";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(699, 193);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 46);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1212, 613);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ CardsDBZ_Client/Hubs/PositionHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CardsDBZ_Server.Hubs
+{
+    public class PositionHub : Hub
+    {
+        public async Task SendPosition(int left, int top)
+        {
+            await Clients.Others.SendAsync("ReceivePosition", left, top);
+        }
+    }
+}
