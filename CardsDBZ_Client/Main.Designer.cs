e9adb05cd417530ed4415a1d396812a3ed5ce39e
AngelSony <angelfol@outlook.com>
2025-09-30 08:07:39 -0300

    Updates UI to show game tables

--- CardsDBZ_Client/Main.Designer.cs
+++ CardsDBZ_Client/Main.Designer.cs
@@ -30,15 +30,18 @@
         {
             components = new System.ComponentModel.Container();
             tableLayoutPanel1 = new TableLayoutPanel();
-            txtPlayerList = new TextBox();
             tableLayoutPanel2 = new TableLayoutPanel();
-            btnJoin = new Button();
+            btnConnect = new Button();
             label2 = new Label();
             txtName = new TextBox();
+            tlpGameTables = new TableLayoutPanel();
+            lblGameTables = new Label();
+            btnUpdate = new Button();
+            txtPlayerList = new TextBox();
             timePlayerListUpdate = new System.Windows.Forms.Timer(components);
-            tableLayoutPanel3 = new TableLayoutPanel();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
+            tlpGameTables.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -49,9 +52,9 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 10F));
-            tableLayoutPanel1.Controls.Add(txtPlayerList, 2, 1);
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 1);
-            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 3, 1);
+            tableLayoutPanel1.Controls.Add(tlpGameTables, 3, 1);
+            tableLayoutPanel1.Controls.Add(txtPlayerList, 2, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
             tableLayoutPanel1.Location = new Point(0, 0);
@@ -61,19 +64,8 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 10F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new Size(1970, 1042);
+            tableLayoutPanel1.Size = new Size(1024, 986);
             tableLayoutPanel1.TabIndex = 0;
-            // 
-            // txtPlayerList
-            // 
-            txtPlayerList.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            txtPlayerList.Location = new Point(413, 13);
-            txtPlayerList.Multiline = true;
-            txtPlayerList.Name = "txtPlayerList";
-            txtPlayerList.ReadOnly = true;
-            txtPlayerList.Size = new Size(194, 1016);
-            txtPlayerList.TabIndex = 2;
-            txtPlayerList.Text = "Conéctate con tu nombre para comenzar";
             // 
             // tableLayoutPanel2
             // 
@@ -81,7 +73,7 @@
             tableLayoutPanel2.ColumnCount = 2;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle());
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.Controls.Add(btnJoin, 0, 1);
+            tableLayoutPanel2.Controls.Add(btnConnect, 0, 1);
             tableLayoutPanel2.Controls.Add(label2, 0, 0);
             tableLayoutPanel2.Controls.Add(txtName, 1, 0);
             tableLayoutPanel2.Location = new Point(13, 13);
@@ -90,21 +82,21 @@
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.Size = new Size(394, 1016);
-            tableLayoutPanel2.TabIndex = 3;
+            tableLayoutPanel2.Size = new Size(394, 960);
+            tableLayoutPanel2.TabIndex = 1;
             // 
-            // btnJoin
+            // btnConnect
             // 
-            btnJoin.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            btnJoin.AutoSize = true;
-            tableLayoutPanel2.SetColumnSpan(btnJoin, 2);
-            btnJoin.Location = new Point(3, 48);
-            btnJoin.Name = "btnJoin";
-            btnJoin.Size = new Size(388, 54);
-            btnJoin.TabIndex = 0;
-            btnJoin.Text = "Conectarse";
-            btnJoin.UseVisualStyleBackColor = true;
-            btnJoin.Click += btnJoin_Click;
+            btnConnect.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnConnect.AutoSize = true;
+            tableLayoutPanel2.SetColumnSpan(btnConnect, 2);
+            btnConnect.Location = new Point(3, 48);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(388, 54);
+            btnConnect.TabIndex = 1;
+            btnConnect.Text = "Conectarse";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
             // 
             // label2
             // 
@@ -123,33 +115,73 @@
             txtName.Location = new Point(116, 3);
             txtName.Name = "txtName";
             txtName.Size = new Size(275, 39);
-            txtName.TabIndex = 2;
+            txtName.TabIndex = 0;
+            // 
+            // tlpGameTables
+            // 
+            tlpGameTables.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tlpGameTables.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tlpGameTables.ColumnCount = 3;
+            tlpGameTables.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200F));
+            tlpGameTables.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlpGameTables.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 60F));
+            tlpGameTables.Controls.Add(lblGameTables, 0, 0);
+            tlpGameTables.Controls.Add(btnUpdate, 2, 0);
+            tlpGameTables.Location = new Point(613, 13);
+            tlpGameTables.Name = "tlpGameTables";
+            tlpGameTables.RowCount = 2;
+            tlpGameTables.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tlpGameTables.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tlpGameTables.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tlpGameTables.Size = new Size(398, 960);
+            tlpGameTables.TabIndex = 3;
+            // 
+            // lblGameTables
+            // 
+            lblGameTables.AutoSize = true;
+            tlpGameTables.SetColumnSpan(lblGameTables, 2);
+            lblGameTables.Dock = DockStyle.Fill;
+            lblGameTables.Location = new Point(4, 1);
+            lblGameTables.Name = "lblGameTables";
+            lblGameTables.Size = new Size(329, 60);
+            lblGameTables.TabIndex = 1;
+            lblGameTables.Text = "Mesas de Juego";
+            lblGameTables.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackgroundImage = Properties.Resources.updateIcon;
+            btnUpdate.BackgroundImageLayout = ImageLayout.Zoom;
+            btnUpdate.Dock = DockStyle.Fill;
+            btnUpdate.Location = new Point(340, 4);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(54, 54);
+            btnUpdate.TabIndex = 0;
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // txtPlayerList
+            // 
+            txtPlayerList.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtPlayerList.Location = new Point(413, 13);
+            txtPlayerList.Multiline = true;
+            txtPlayerList.Name = "txtPlayerList";
+            txtPlayerList.ReadOnly = true;
+            txtPlayerList.Size = new Size(194, 960);
+            txtPlayerList.TabIndex = 2;
+            txtPlayerList.TabStop = false;
+            txtPlayerList.Text = "Conéctate con tu nombre para comenzar";
             // 
             // timePlayerListUpdate
             // 
             timePlayerListUpdate.Interval = 5000;
             timePlayerListUpdate.Tick += timePlayerListUpdate_Tick;
             // 
-            // tableLayoutPanel3
-            // 
-            tableLayoutPanel3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            tableLayoutPanel3.ColumnCount = 2;
-            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 9.523809F));
-            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 90.47619F));
-            tableLayoutPanel3.Location = new Point(613, 13);
-            tableLayoutPanel3.Name = "tableLayoutPanel3";
-            tableLayoutPanel3.RowCount = 3;
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 313F));
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 654F));
-            tableLayoutPanel3.Size = new Size(1344, 1016);
-            tableLayoutPanel3.TabIndex = 4;
-            // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1970, 1042);
+            ClientSize = new Size(1024, 986);
             Controls.Add(tableLayoutPanel1);
             Name = "Main";
             Text = "Main Menu";
@@ -158,18 +190,22 @@
             tableLayoutPanel1.PerformLayout();
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
+            tlpGameTables.ResumeLayout(false);
+            tlpGameTables.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private Button btnJoin;
+        private Button btnConnect;
         private TextBox txtPlayerList;
         private TableLayoutPanel tableLayoutPanel2;
         private Label label2;
         private TextBox txtName;
         private System.Windows.Forms.Timer timePlayerListUpdate;
-        private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tlpGameTables;
+        private Label lblGameTables;
+        private Button btnUpdate;
     }
 }
--- CardsDBZ_Client/Main.cs
+++ CardsDBZ_Client/Main.cs
@@ -1,7 +1,9 @@
+using CardsDBZ_Server;
 using Microsoft.AspNetCore.SignalR.Client;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace CardsDBZ_Client
 {
@@ -11,6 +13,7 @@ namespace CardsDBZ_Client
         private string _url = "https://localhost:7012/lobby";
         private HubConnection _connection;
         private string _playerName;
+        private List<TableData> _tableList;
         public Main()
         {
             InitializeComponent();
@@ -32,7 +35,7 @@ namespace CardsDBZ_Client
                 {
                     txtName.ReadOnly = false;
                     txtPlayerList.Text = "Se perdió la cconexión";
-                    btnJoin.Enabled = true;
+                    btnConnect.Enabled = true;
                 }
                 ));
             };
@@ -45,6 +48,13 @@ namespace CardsDBZ_Client
                     txtPlayerList.Text = "Jugadores:" + Environment.NewLine + playerList
                 ));
             });
+            _connection.On<List<TableData>>("TableListUpdate", (tableList) =>
+            {
+                Invoke(new Action(() =>
+                    UpdateTableList(tableList)
+                ));
+            });
+
             timePlayerListUpdate.Start();
         }
         private async Task Connect(string playerName)
@@ -56,21 +66,20 @@ namespace CardsDBZ_Client
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex.Message);
                 MessageBox.Show("Error al conectarse al servidor", "Connection error");
                 txtName.ReadOnly = false;
-                btnJoin.Enabled = true;
+                btnConnect.Enabled = true;
                 txtPlayerList.Text = "Error al conectar";
             }
         }
-
         private void Main_Load(object sender, EventArgs e)
         {
             BuildConnection();
         }
-
-        private async void btnJoin_Click(object sender, EventArgs e)
+        private async void btnConnect_Click(object sender, EventArgs e)
         {
-            btnJoin.Enabled = false;
+            btnConnect.Enabled = false;
             string playerName = txtName.Text.Trim();
             if (!playerName.Equals(""))
             {
@@ -83,14 +92,65 @@ namespace CardsDBZ_Client
             {
                 MessageBox.Show("Debes tener un Nombre válido para conectarte");
                 txtName.Focus();
-                btnJoin.Enabled = true;
+                btnConnect.Enabled = true;
             }
         }
         private async void timePlayerListUpdate_Tick(object sender, EventArgs e)
         {
-            if(_connection.State == HubConnectionState.Connected)
+            if (_connection.State == HubConnectionState.Connected)
             {
                 await _connection.InvokeAsync("PlayerListUpdate");
+            }
+        }
+        private void UpdateTableList(List<TableData> tableList)
+        {
+            //Reset talpGameTables to title only
+            tlpGameTables.Controls.Clear();
+            tlpGameTables.RowStyles.Clear();
+            tlpGameTables.RowCount = 1;
+            tlpGameTables.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tlpGameTables.Controls.Add(lblGameTables, 0, 0);
+            tlpGameTables.Controls.Add(btnUpdate, 2, 0);
+
+            //Adding rows for each GameTable
+            foreach (TableData table in tableList)
+            {
+                tlpGameTables.RowStyles.Add(new RowStyle(SizeType.AutoSize, 80F));
+                tlpGameTables.RowCount++;
+
+                Label lblTable = new Label();
+                    lblTable.AutoSize = true;
+                    lblTable.Dock = DockStyle.Fill;
+                    lblTable.Name = "lblTable" + table.TableId;
+                    lblTable.Text = $"Mesa {table.TableId}\r\nJugadores ({table.PlayerCount}/2)";
+                    lblTable.TextAlign = ContentAlignment.MiddleCenter;
+                tlpGameTables.Controls.Add(lblTable, 0, tlpGameTables.RowCount - 1);
+
+                Button btnJoin = new Button();
+                    btnJoin.Dock = DockStyle.Fill;
+                    btnJoin.Name = "btnJoin" + table.TableId;
+                    btnJoin.TabIndex = table.TableId + 1;
+                    btnJoin.Text = "Unirse";
+                    //btnJoin.UseVisualStyleBackColor = true;
+                    btnJoin.Click += btnJoin_Click;
+                tlpGameTables.Controls.Add(btnJoin, 1, tlpGameTables.RowCount - 1);
+                tlpGameTables.SetColumnSpan(btnJoin, 2);
+                    
+                
+            }
+            
+            tlpGameTables.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tlpGameTables.RowCount++;
+        }
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+
+        }
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (_connection.State == HubConnectionState.Connected)
+            {
+                await _connection.InvokeAsync("TableListUpdate");
             }
         }
     }
